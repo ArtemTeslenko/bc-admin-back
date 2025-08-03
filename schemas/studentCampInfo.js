38625dc831014802742703cbc812009744e4fef1
Artem <104884717+ArtemTeslenko@users.noreply.github.com>
2025-08-03 10:31:27 +0300

    main: fix joi schema

--- schemas/studentCampInfo.js
+++ schemas/studentCampInfo.js
@@ -1,16 +1,16 @@
 const Joi = require("joi");
 
 const studentCampInfo = Joi.object({
-  group: Joi.string(),
-  materialsTaught: Joi.string(),
-  finalTestResult: Joi.string(),
-  topicsCovered: Joi.string(),
-  workingMode: Joi.string(),
-  speaking: Joi.string(),
-  reading: Joi.string(),
-  listening: Joi.string(),
-  speakingOfWriting: Joi.string(),
-  additionalInfo: Joi.string(),
+  group: Joi.string().allow(""),
+  materialsTaught: Joi.string().allow(""),
+  finalTestResult: Joi.string().allow(""),
+  topicsCovered: Joi.string().allow(""),
+  workingMode: Joi.string().allow(""),
+  speaking: Joi.string().allow(""),
+  reading: Joi.string().allow(""),
+  listening: Joi.string().allow(""),
+  speakingOfWriting: Joi.string().allow(""),
+  additionalInfo: Joi.string().allow(""),
 });
 
 module.exports = studentCampInfo;
